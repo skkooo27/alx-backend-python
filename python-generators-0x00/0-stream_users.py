851380ce21bb89d4f374c5dedb9c5ce1b519d639
sharlinekerubo27@gmail.com <sharlinekerubo27@gmail.com>
2025-07-03 23:50:54 +0300

    updates

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,22 @@
+# 1-batch_processing.py
+import seed
+
+def stream_users_in_batches(batch_size):
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+    
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if not batch:
+            break
+        yield batch
+
+    cursor.close()
+    connection.close()
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user['age'] > 25:
+                print(user)
