f9dbebce4793136ac8247f97dd7cd9c937a3ded6
sharlinekerubo27@gmail.com <sharlinekerubo27@gmail.com>
2025-07-04 00:07:46 +0300

    Task 2: Batch streaming of users and filter age > 25

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -5,18 +5,18 @@ def stream_users_in_batches(batch_size):
     connection = seed.connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
     cursor.execute("SELECT * FROM user_data")
-    
+
     while True:
-        rows = cursor.fetchmany(batch_size)
+        rows = cursor.fetchmany(batch_size)  # gets `batch_size` rows
         if not rows:
             break
-        yield rows
+        yield rows  # yield the whole batch
 
     cursor.close()
     connection.close()
 
 def batch_processing(batch_size):
-    for batch in stream_users_in_batches(batch_size):
-        for user in batch:
+    for batch in stream_users_in_batches(batch_size):  # loop over batches
+        for user in batch:  # loop over users in each batch
             if user['age'] > 25:
                 print(user)
