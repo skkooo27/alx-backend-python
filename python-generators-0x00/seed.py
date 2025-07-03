7bd37cb7affa1d7c1c6cd8fa80a93de40ddc240f
sharlinekerubo27@gmail.com <sharlinekerubo27@gmail.com>
2025-07-03 23:46:07 +0300

    add seed.py and README file

---
+++ python-generators-0x00/seed.py
@@ -0,0 +1,16 @@
+# 0-stream_users.py
+import seed
+
+def stream_users():
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+    
+    while True:
+        row = cursor.fetchone()
+        if row is None:
+            break
+        yield row
+
+    cursor.close()
+    connection.close()
