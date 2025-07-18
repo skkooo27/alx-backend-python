c6cb84c55a81d977dba4233a7e5ad4f777fe1000
sharlinekerubo27@gmail.com <sharlinekerubo27@gmail.com>
2025-07-18 11:06:12 +0300

    Fix E501 (Line Too Long)

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -63,7 +63,9 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+        with patch.object(
+            TestClass, 'a_method', return_value=42
+            ) as mock_method:
             test_obj = TestClass()
             result1 = test_obj.a_property
             result2 = test_obj.a_property
