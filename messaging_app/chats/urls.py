6f8d455b41eae6f73bf18bb70e99b06f652720f1
sharlinekerubo27@gmail.com <sharlinekerubo27@gmail.com>
2025-07-18 13:25:01 +0300

    correction on url file

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers 
 from chats import views
 
-router = NestedDefaultRouter()
-
-router = DefaultRouter()
+router = routers.DefaultRouter()  
 router.register(r'conversations', views.ConversationViewSet)
 router.register(r'messages', views.MessageViewSet)
 
