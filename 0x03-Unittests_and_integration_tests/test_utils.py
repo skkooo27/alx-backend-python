9c1e0be5b0ac91a52edcc6945719d3caf2aff18f
sharlinekerubo27@gmail.com <sharlinekerubo27@gmail.com>
2025-07-18 11:12:09 +0300

    fixed the identation

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -64,8 +64,8 @@ class TestMemoize(unittest.TestCase):
                 return self.a_method()
 
         with patch.object(
-            TestClass, 'a_method', return_value=42
-            ) as mock_method:
+                TestClass, 'a_method', return_value=42
+        ) as mock_method:
             test_obj = TestClass()
             result1 = test_obj.a_property
             result2 = test_obj.a_property
